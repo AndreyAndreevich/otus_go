eefd12622ac85bde2fd1b0865852b6766ea1232d
AndreyAndreevich <l.a.r.p@yandex.ru>
2019-12-05 18:52:32 +0300

    Is worked

---
+++ hw-01/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(time)
+}

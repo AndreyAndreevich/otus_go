e12ce862f35a1754bbe9e085a8b101d0f66b8729
AndreyAndreevich <l.a.r.p@yandex.ru>
2020-03-29 20:49:07 +0300

    Added scheduler

--- calendar/internal/calendar/scheduler.go
+++ calendar/internal/calendar/scheduler.go
@@ -3,9 +3,36 @@ package calendar
 import (
 	"context"
 	"time"
+
+	"go.uber.org/zap"
 )
 
 // Schedule publish events
 func (c *Calendar) Schedule(ctx context.Context, duration time.Duration) error {
-	return nil
+	last := time.Unix(0, 0)
+	timeChan := time.Tick(duration)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-timeChan:
+			sDuration := time.Now().Sub(last)
+			events, err := c.storage.GetEventsInTime(ctx, last, sDuration)
+			if err != nil {
+				c.logger.Error("storage GetEventsInTime error", zap.Error(err))
+				return err
+			}
+
+			c.logger.Debug("publish", zap.Int("events length", len(events)))
+
+			for _, event := range events {
+				if err := c.publisher.Publish(event); err != nil {
+					c.logger.Error("publish  error", zap.Error(err))
+					return err
+				}
+			}
+
+			last = last.Add(sDuration)
+		}
+	}
 }
